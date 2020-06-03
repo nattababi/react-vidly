a1903c60c3cb44ea544017a6451255bc0e48671a
nattababi <nataly.babicheva@gmail.com>
2020-06-02 20:56:58 -0400

    code refactoring

--- src/components/movieDetails.jsx
+++ src/components/movieDetails.jsx
@@ -1,60 +1,11 @@
 import React, { Component } from 'react';
-// import NewMovieForm from './newMovieForm';
- import { getMovie, getMovies } from '../services/fakeMovieService';
-// import NotFound from './notFound';
-
+import { getMovie } from '../services/fakeMovieService';
 import Form from './form';
 import Joi from 'joi-browser';
 import { saveMovie } from '../services/fakeMovieService';
 import * as genresAPI from '../services/fakeGenreService';
 
 class MovieDetails extends Form {
-  // state = {}
-
-  // handleSave = () => {
-  //   // Navigate to /products
-  //   //this.props.history.replace('/movies'); // no history
-  //   this.props.history.push('/movies'); // with history
-
-  // };
-
-  // render() {
-
-  //   //find movie with the given Id
-  //   const id = this.props.match.params.id;
-  //   const movieInDb = getMovie(id);
-
-  //   console.log(movieInDb);
-
-  //   if (movieInDb) {
-  //     //console.log('movie is found');
-  //     return (
-  //       <NewMovieForm
-  //         _id={movieInDb._id}
-  //         title={movieInDb.title}
-  //         genre={movieInDb.genre}
-  //         numberInStock={movieInDb.numberInStock}
-  //         dailyRentalRate={movieInDb.dailyRentalRate}
-  //         history={this.props.history}
-  //       />
-  //     );
-  //   }
-  //   else {
-
-  //     // const movies = getMovies();
-  //     // console.log(movies);
-      
-  //     //redirect to not_found page
-  //     //this.props.history.push('/not-found'); // with history
-      
-  //     return(
-  //       <NotFound/>
-  //     );
-
-  //   }
-
-
-  // }
 
   state = {
     data: { title: '', genre: '', numberInStock: '', dailyRentalRate: '', _id: '' },
